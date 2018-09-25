287e95bc1a2d57b2bc79a5ee39ea41f473697977
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 04:13:29 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,59 @@
+# oo
+# {:ruby=>{:type=>"interpreted"}, :javascript=>{:type=>"interpreted"}, :python=>{:type=>"interpreted"}, :java=>{:type=>"compiled"}}
+# functional
+# {:clojure=>{:type=>"compiled"}, :erlang=>{:type=>"compiled"}, :scala=>{:type=>"compiled"}, :javascript=>{:type=>"interpreted"}}
+
 def reformat_languages(languages)
-  # your code here
+  languages.each do |key, val|
+    puts key
+    puts '^^^^^^^^^^^^^^^^^^^^^^'
+    puts val
+    val.keys.each do |k|
+      puts 'GGGGGGGGGGGGGGGGGGGGGGGG'
+      puts (:style => key.to_s)
+      puts 'GGGGGGGGGGGGGGGGGGGGGGGG'
+    end 
+
+  end 
+  # puts languages
+  # languages
 end
+
+languages = 
+      {
+      :oo => {
+        :ruby => {
+          :type => "interpreted"
+        },
+        :javascript => {
+          :type => "interpreted"
+        },
+        :python => {
+          :type => "interpreted"
+        },
+        :java => {
+          :type => "compiled"
+        }
+      },
+      :functional => {
+        :clojure => {
+          :type => "compiled"
+        },
+        :erlang => {
+          :type => "compiled"
+        },
+        :scala => {
+          :type => "compiled"
+        },
+        :javascript => {
+          :type => "interpreted"
+        }
+     
+      }
+    }
+
+
+
+
+reformat_languages(languages)
+
